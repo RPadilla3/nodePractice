250c60f2cb5fdb8b1e2c28146a33e7a77f4ec2a2
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-25 18:38:59 -0500

    Created package.json file with basic node dependencies, and required dependencies in my server.js file.

---
+++ api/server.js
@@ -0,0 +1,10 @@
+console.log('server is running');
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = app.listen(3000, listening);
+
+function listening() {
+  console.log('Listening...');
+}
