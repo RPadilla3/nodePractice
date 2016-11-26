e7fae80f310f770a6f1740b3d0bffe7989195a12
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-25 19:41:24 -0500

    Added a POST method onto my form in order to add a new person, with first name last name and email.

--- api/server.js
+++ api/server.js
@@ -5,12 +5,10 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var app = express();
 
-// var logger = function(req, res, next){
-//   console.log('logging...');
-//   next();
-// }
-// app.use(logger);
+// View Engine
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
@@ -19,25 +17,43 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Set Static path
 app.use(express.static(path.join(__dirname, 'public')));
 
-var people = [
+var users = [
     {
-      name: 'Rodolfo',
-      age: 21
+      id: 1,
+      first_name: 'Rodolfo',
+      last_name: 'Padilla',
+      email: 'Padilla_r95@Rocketmail.com'
     },
-  {
-      name: 'Isa',
-      age: 13
-  },
-  {
-      name: 'Luisa',
-      age: 48
+    {
+      id: 2,
+      first_name: 'Isabella',
+      last_name: 'Padilla',
+      email: 'IPadilla@fake.com'
+    },
+    {
+      id: 3,
+      first_name: 'Luisa',
+      last_name: 'Gutierrez',
+      email: 'Lguti@fake.com'
     }
-]
+];
 
 var server = app.listen(3000, listening);
 
 app.get('/', function(req, res){
-    res.json(people);
+    res.render('index', {
+      title: 'Customers',
+      users: users
+    });
+});
+
+app.post('/users/add', function(req, res){
+  var newUser = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email
+  }
+  console.log(newUser);
 });
 
 function listening() {
